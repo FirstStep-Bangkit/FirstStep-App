1e3e093b43e2e5d4bbe4cc4b10f17ac67a29e17a
Yunian Edwi Risnawa <yunianedwi01@gmail.com>
2023-06-03 13:04:24 +0700

    Add Api Service

--- app/src/main/java/com/example/firststepapp/api/ApiService.kt
+++ app/src/main/java/com/example/firststepapp/api/ApiService.kt
@@ -1,5 +1,27 @@
 package com.example.firststepapp.api
 
+import com.example.firststepapp.api.response.LoginResponse
+import com.example.firststepapp.api.response.RegisterResponse
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface ApiService {
 
+    @POST("register")
+    @FormUrlEncoded
+    fun register(
+        @Field("frontName") frontName: String,
+        @Field("lastName") lastName : String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<RegisterResponse>
+
+    @POST("login")
+    @FormUrlEncoded
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<LoginResponse>
 }
