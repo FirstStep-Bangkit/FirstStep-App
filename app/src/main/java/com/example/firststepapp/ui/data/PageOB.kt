d406c8b6ebf038d99dc5cf863922f2e801785b1d
rizkyyazk <rizkyyazk@gmail.com>
2023-06-15 08:28:16 +0700

    Add Assets and Update Onboarding

--- app/src/main/java/com/example/firststepapp/ui/data/PageOB.kt
+++ app/src/main/java/com/example/firststepapp/ui/data/PageOB.kt
@@ -9,19 +9,19 @@ sealed class PageOB(
     val description: String
 ) {
     object First : PageOB(
-        image = R.drawable.onboarding_one,
+        image = R.drawable.welcome,
         title = "Welcome",
         description = "Selamat datang diaplikasi FirstStep. Aplikasi untuk mengetahui kepribadian Kamu."
     )
 
     object Second : PageOB(
-        image = R.drawable.onboarding_one,
+        image = R.drawable.find,
         title = "Know your personality ",
         description = "Cari tahu kepribadian kamu disini dengan mengikuti beberapa tes. Dan lebih jauh mengenal diri kamu sendiri."
     )
 
     object Third : PageOB(
-        image = R.drawable.onboarding_one,
+        image = R.drawable.started,
         title = "Lets Started",
         description = "Mulai Gaskeun."
     )
